ef6c344939266517c5b8b26a2d551193edaa33d0
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-01-28 08:50:12 -0300

    Modo operação completo e calibração incompleto

--- app/src/main/java/me/aflak/heroicuidador/activity/ApresentacaoCalibracaoActivity.java
+++ app/src/main/java/me/aflak/heroicuidador/activity/ApresentacaoCalibracaoActivity.java
@@ -2,48 +2,35 @@ package me.aflak.heroicuidador.activity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import me.aflak.bluetooth.Bluetooth;
 import me.aflak.heroicuidador.R;
 
-public class HomeActivity extends AppCompatActivity implements Bluetooth.CommunicationCallback {
+public class ApresentacaoCalibracaoActivity extends AppCompatActivity implements Bluetooth.CommunicationCallback {
 
     // Bluetooth
     private String name;
     private Bluetooth b;
     private boolean registered = false;
 
-    private TextView text;
-
-    // Navegação
-    private TextView textViewOperacao;
-    private TextView textViewCalibracao;
-
+    private Button buttonStartCalibracao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_apresentacao_calibracao);
 
-        textViewOperacao = findViewById(R.id.textViewOperacao);
-        textViewCalibracao = findViewById(R.id.textViewCalibracao);
-
-        text = findViewById(R.id.text);
+        buttonStartCalibracao = findViewById(R.id.buttonStartCalibracao);
 
 
         // Bluetooth
@@ -65,87 +52,21 @@ public class HomeActivity extends AppCompatActivity implements Bluetooth.Communi
         registered = true;
 
 
-        // Navegação
 
-        textViewCalibracao.setOnClickListener(new View.OnClickListener() {
+        buttonStartCalibracao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ApresentacaoCalibracaoActivity.class);
+                Intent intent = new Intent(getApplicationContext(), CalibracaoActivity.class);
                 intent.putExtra("pos", position);
 
                 startActivity(intent);
-            }
-        });
-
-        textViewOperacao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), OperacaoActivity.class);
-                intent.putExtra("pos", position);
-
-                startActivity(intent);
-            }
-        });
-
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (registered) {
-            unregisterReceiver(mReceiver);
-            registered = false;
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.close:
-                b.removeCommunicationCallback();
-                b.disconnect();
-                Intent intent = new Intent(this, Select.class);
-                startActivity(intent);
-                finish();
-                return true;
-
-            case R.id.rate:
-                Uri uri = Uri.parse("market://details?id=" + this.getPackageName());
-                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
-                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                try {
-                    startActivity(goToMarket);
-                } catch (ActivityNotFoundException e) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("http://play.google.com/store/apps/details?id=" + this.getPackageName())));
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public void Display(final String s) {
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                //text.append(s + "\n");
-                //scrollView.fullScroll(View.FOCUS_DOWN);
             }
         });
     }
 
     @Override
     public void onConnect(BluetoothDevice device) {
-       // Display("Conectado " + device.getName() + " - " + device.getAddress());
+        Display("Conectado " + device.getName() + " - " + device.getAddress());
         Toast.makeText(getApplicationContext(),  "Connected", Toast.LENGTH_SHORT).show();
 
         this.runOnUiThread(new Runnable() {
@@ -153,15 +74,6 @@ public class HomeActivity extends AppCompatActivity implements Bluetooth.Communi
             public void run() {
             }
         });
-    }
-
-    @Override
-    public void onBackPressed() {
-        b.removeCommunicationCallback();
-        b.disconnect();
-        Intent intent = new Intent(this, Select.class);
-        startActivity(intent);
-        finish();
     }
 
     @Override
@@ -173,18 +85,20 @@ public class HomeActivity extends AppCompatActivity implements Bluetooth.Communi
 
     @Override
     public void onMessage(String message) {
-        // Troca de mensagem
+        Display(message);
     }
 
     @Override
     public void onError(String message) {
-        Display("Error: "+message);
+        Display("Error: " + message);
     }
+
 
     @Override
     public void onConnectError(final BluetoothDevice device, String message) {
-        Display("Error: "+message);
+        Display("Error: " + message);
         Display("Trying again in 3 sec.");
+
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -199,6 +113,14 @@ public class HomeActivity extends AppCompatActivity implements Bluetooth.Communi
         });
     }
 
+    public void Display(final String s) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -206,7 +128,7 @@ public class HomeActivity extends AppCompatActivity implements Bluetooth.Communi
 
             if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
                 final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
-                Intent intent1 = new Intent(HomeActivity.this, Select.class);
+                Intent intent1 = new Intent(ApresentacaoCalibracaoActivity.this, Select.class);
 
                 switch (state) {
                     case BluetoothAdapter.STATE_OFF:
--- app/src/main/java/me/aflak/heroicuidador/activity/CalibracaoActivity.java
+++ app/src/main/java/me/aflak/heroicuidador/activity/CalibracaoActivity.java
@@ -1,14 +1,136 @@
 package me.aflak.heroicuidador.activity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import me.aflak.bluetooth.Bluetooth;
 import me.aflak.heroicuidador.R;
 
-public class CalibracaoActivity extends AppCompatActivity {
+public class CalibracaoActivity extends AppCompatActivity implements Bluetooth.CommunicationCallback {
+
+    // Bluetooth
+    private String name;
+    private Bluetooth b;
+    private boolean registered = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibracao);
+
+        // Bluetooth
+
+        b = new Bluetooth(this);
+        b.enableBluetooth();
+
+        b.setCommunicationCallback(this);
+
+        final int position = getIntent().getExtras().getInt("pos");
+        name = b.getPairedDevices().get(position).getName();
+
+        Toast.makeText(getApplicationContext(),  "Conectando...", Toast.LENGTH_SHORT).show();
+        //  Display("Connecting...");
+        b.connectToDevice(b.getPairedDevices().get(position));
+
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mReceiver, filter);
+        registered = true;
+
     }
+
+    @Override
+    public void onConnect(BluetoothDevice device) {
+        Display("Conectado " + device.getName() + " - " + device.getAddress());
+        Toast.makeText(getApplicationContext(),  "Connected", Toast.LENGTH_SHORT).show();
+
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnect(BluetoothDevice device, String message) {
+        Display("Disconnected!");
+        Display("Connecting again...");
+        b.connectToDevice(device);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        Display(message);
+    }
+
+    @Override
+    public void onError(String message) {
+        Display("Error: " + message);
+    }
+
+
+    @Override
+    public void onConnectError(final BluetoothDevice device, String message) {
+        Display("Error: " + message);
+        Display("Trying again in 3 sec.");
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.connectToDevice(device);
+                    }
+                }, 2000);
+            }
+        });
+    }
+
+    public void Display(final String s) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                Intent intent1 = new Intent(CalibracaoActivity.this, Select.class);
+
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered = false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered = false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                }
+            }
+        }
+    };
 }
