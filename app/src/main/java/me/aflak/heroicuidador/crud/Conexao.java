e983c7e082bc7aef98d5d39aba79468ce51af428
Lucas Morais de Araújo <lma6@cin.ufpe.br>
2021-05-18 20:51:53 -0300

    cadastro idoso

--- app/src/main/java/me/aflak/heroicuidador/crud/Conexao.java
+++ app/src/main/java/me/aflak/heroicuidador/crud/Conexao.java
@@ -19,11 +19,14 @@ public class Conexao  extends  SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table cuidador(id integer primary key autoincrement, "+
                 "nome varchar(50), email varchar(50), altura varchar(50),idade varchar(50), sexo varchar(50), peso varchar(50), senha varchar(50) )");
+        db.execSQL("create table idoso(id integer primary key autoincrement, "+
+                "nomeIdoso varchar(50), alturaIdoso varchar(50),idadeIdoso varchar(50), sexoIdoso varchar(50), pesoIdoso varchar(50))");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS cuidador");
+        db.execSQL("DROP TABLE IF EXISTS idoso");
         onCreate(db);
     }
 
@@ -38,6 +41,18 @@ public class Conexao  extends  SQLiteOpenHelper{
         values.put("senha", cuidador.getSenha());
         values.put("sexo", cuidador.getSexo());
         long result = db.insert("cuidador",null,values);
+        return result;
+    }
+
+    public long CriarIdoso( Idoso idoso){
+        SQLiteDatabase db =getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("nomeIdoso", idoso.getNomeIdoso());
+        values.put("alturaIdoso", idoso.getAlturaIdoso());
+        values.put("idadeIdoso", idoso.getIdadeIdoso());
+        values.put("pesoIdoso", idoso.getPesoIdoso());
+        values.put("sexoIdoso", idoso.getSexoIdoso());
+        long result = db.insert("idoso",null,values);
         return result;
     }
 
